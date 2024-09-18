af30e03e699c3d19eab508c76d3a64c5b7634c66
Royhan <royhankeren12@gmail.com>
2024-09-18 22:58:31 +0700

    add register

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Http\Resources\LoginResource;
+use App\Models\User;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 
 class AuthController extends Controller
@@ -44,14 +46,69 @@ class AuthController extends Controller
                 $token
             )
         ], 200);
-
     }
 
-    public function me() {
+    public function me()
+    {
         return response()->json([
             'success' => true,
             'message' => 'Login Succesfully',
             'data' =>  auth('api')->user()
+        ], 200);
+    }
+
+    public function register(Request $request)
+    {
+
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'email'     => 'required|unique:users',
+            'password'  => 'required'
+        ]);
+
+        //if validation fails
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        };
+
+        $create = User::create([
+            'name'  => $request->name,
+            'email'     => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        $credentials = [
+            'email' => $request->email,
+            'password' => $request->password,
+        ];
+
+        $token = auth('api')->claims([
+            'exp' => Carbon::now()->addSeconds((int)config('jwt.ttl_in_seconds'))->timestamp,
+        ])->attempt($credentials, [
+            'exp' => Carbon::now()->addSeconds((int)config('jwt.ttl_in_seconds'))->timestamp,
+        ]);
+
+        if (!$token) {
+            throw new \Exception('Email or password does not match.', 401);
+        }
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Login Succesfully',
+            'data' => new LoginResource(
+                $token
+            )
+        ], 200);
+    }
+
+    public function logout()
+    {
+        auth('api')->logout();
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Logout Succesfully',
+            'data' => null,
         ], 200);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -9,6 +9,8 @@ Route::get('/user', function (Request $request) {
 })->middleware('auth:sanctum');
 
 Route::post('/login', [AuthController::class, 'login']);
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/logout', [AuthController::class, 'logout']);
 
 Route::get('/me', [AuthController::class, 'me'])->middleware('auth:api');
 
